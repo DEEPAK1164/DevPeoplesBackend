184990d527f9cbe991a4282eeb5d10a4c14175d3
Deepak <2001dkmaurya@gmail.com>
2025-06-23 01:03:04 +0530

    routing and request handler

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use("/test",(req, res) => {
- 
-  res.send('Hello, World From Server!');
+app.get("/user/:userId",(req, res) => {
+  res.send(req.params);
 });
 
-const PORT = 3000;
+
+app.post("/user",(req, res) => {
+  res.send("Data saved successfully to DB");
+});
+
+
+
+app.delete("/user",(req, res) => {
+  res.send("Data deleted successfully from DB");
+});
+
+
+const PORT = 7777;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
