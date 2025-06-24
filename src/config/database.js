0156cdf7e0cda32295694f6d0394545dd342eb2d
Deepak <2001dkmaurya@gmail.com>
2025-06-24 22:34:39 +0530

    connected backend with particular db of the cluster using mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://1164dkm:ddNg0R7IDhO4nzYp@namastenode.xfqk0.mongodb.net/");
+    await mongoose.connect("mongodb+srv://1164dkm:ddNg0R7IDhO4nzYp@namastenode.xfqk0.mongodb.net/DevPeoples");
 }
 
 connectDB()
