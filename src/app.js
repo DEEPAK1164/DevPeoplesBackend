4aae012304a353aafe1fb10e1d2371a0d9337888
Deepak <2001dkmaurya@gmail.com>
2025-06-25 07:51:51 +0530

    created UserModel and signup api to post the data into the db

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-
 // Importing the database configuration
 const connectDB = require("./config/database");
+const UserModel = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  try {
+    const userObj = {
+      firstName: "Virat",
+      lastName: "Kohli",
+      emailId: "virat@gmail.com",
+      password: "Virat@123"
+    };
+
+    // Creating a new user instance using the UserModel
+    const user = new UserModel(userObj);
+    // Saving the user to the database
+    await user.save(); // this will return a promise
+    res.send("User created successfully");
+  } catch (err) {
+    // console.error("Signup error:", err.message);
+    res.status(500).send("Error creating user: " + err.message);
+  }
+});
 
 const PORT = 7777;
-
 
 connectDB().then(() => {
   console.log('Database connected successfully');
---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minLength:4
+    },
+    lastName:{
+        type:String
+
+    },
+    emailId:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},
+{
+    timestamps:true,//it tells when user registered
+}
+);
+
+const UserModel=mongoose.model("User",userSchema);
+
+
+module.exports=UserModel;
