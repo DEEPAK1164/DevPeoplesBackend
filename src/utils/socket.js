0141c14f4915e39782c22b5b940dd672b6d7b880
Deepak <2001dkmaurya@gmail.com>
2025-07-10 02:39:41 +0530

    building chat feature

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,18 +5,25 @@ const initializeSocket=(server)=>{
     
 const io=socket(server,{
   cors:{
-    origin:" http://localhost:5173/",
+    origin:"http://localhost:5173",
   }
 })
 
 io.on("connection",(socket)=>{
   // Handle Events
-    socket.on("joinChat",()=>{
-       
+    socket.on("joinChat",({loggedInUserId,toUserId})=>{
+        // whenever chat happens it happens inside the room
+     
+         const roomId=[loggedInUserId,toUserId].sort().join("_");
+         console.log("Joining Room : "+roomId);
+         socket.join(roomId);
+
     })
 
-    socket.on("sendMessage",()=>{
-
+    socket.on("sendMessage",({firstName,loggedInUserId,toUserId,text})=>{
+        const roomId=[loggedInUserId,toUserId].sort().join("_");
+        console.log(firstName+" "+text);
+        io.to(roomId).emit("messageReceived",{firstName,text});
     })
 
     socket.on("disconnect",()=>{})
